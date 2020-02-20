24a2e12c9ad00991f3f036c5bb7da9d4e9c49643
fullere <fullere@hartwick.edu>
2020-02-20 00:33:59 -0500

    Created classes

--- linkedlist.py
+++ linkedlist.py
@@ -1,1 +1,15 @@
 # Elizabeth Fuller
+
+
+class ListNode:
+    def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
+
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def prepend(self,data):
+        self.head = ListNode(data=data, next=self.head)
