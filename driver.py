cf3841e8b78a689473437d7004e7a89a58977a1f
fullere <fullere@hartwick.edu>
2020-02-20 09:18:36 -0500

    Created driver file to create list and add to the list

--- driver.py
+++ driver.py
@@ -1,2 +1,16 @@
-import linkedlist
+# Elizabeth Fuller
+# 2/20/20
+# driver file
 
+from linkedlist import SinglyLinkedList
+
+# creates a singly linked list head
+new_list = SinglyLinkedList()
+print(new_list)
+# adds new nodes to the start of the list
+new_list.prepend(5)
+new_list.prepend(6)
+new_list.prepend('ko')
+new_list.prepend('5 Elements')
+
+print(new_list)
--- linkedlist.py
+++ linkedlist.py
@@ -1,15 +1,28 @@
 # Elizabeth Fuller
+# 2/20/20
+# linked lists class
 
 
-class ListNode:
+class Node:
     def __init__(self, data=None, next=None):
         self.data = data
         self.next = next
+
+    def __repr__(self):
+        return repr(self.data)
 
 
 class SinglyLinkedList:
     def __init__(self):
         self.head = None
 
+    def __repr__(self):
+        nodes = []
+        curr = self.head
+        while curr:
+            nodes.append(repr(curr))
+            curr = curr.next
+        return '['+', '.join(nodes) + ']'
+
     def prepend(self,data):
-        self.head = ListNode(data=data, next=self.head)
+        self.head = Node(data=data, next=self.head)
