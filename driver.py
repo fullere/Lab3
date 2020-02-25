b2570312eea1127ba0f8415457af3353ec383345
fullere <fullere@hartwick.edu>
2020-02-25 09:19:56 -0500

    added new methods

--- driver.py
+++ driver.py
@@ -5,12 +5,44 @@
 from linkedlist import SinglyLinkedList
 
 # creates a singly linked list head
+print("Lets make a linked list:")
 new_list = SinglyLinkedList()
 print(new_list)
 # adds new nodes to the start of the list
+print("Lets add 5, 6, 7, and 8 to the head of the list")
 new_list.prepend(5)
 new_list.prepend(6)
-new_list.prepend('ko')
-new_list.prepend('5 Elements')
+new_list.prepend(7)
+new_list.prepend(8)
+print("Lets add 'ko' and '5 Elements' to the end of the list")
+new_list.add_to_end('ko')
+new_list.add_to_end('5 Elements')
+print("Here is the list now")
+print(new_list)
 
+print(new_list.remove_from_head())
+print("Here is the list now")
+print(new_list)
+
+print(new_list.remove_from_end())
+print("Here is the list now")
+print(new_list)
+
+print("Lets find out if 5 is in the list")
+print(new_list.search(5))
+print("Lets find out if 10 is in the list")
+print(new_list.search(10))
+
+print("Lets remove the number 6 from the list")
+print(new_list.remove(6))
+print("Here is the list now")
+print(new_list)
+
+print("Lets remove the number 7 from the list")
+print(new_list.remove(7))
+print("Here is the list now")
+print(new_list)
+
+print("Lets clear the list:")
+new_list.clear()
 print(new_list)
--- linkedlist.py
+++ linkedlist.py
@@ -22,7 +22,55 @@ class SinglyLinkedList:
         while curr:
             nodes.append(repr(curr))
             curr = curr.next
-        return '['+', '.join(nodes) + ']'
+        return '[' + ', '.join(nodes) + ']'
 
-    def prepend(self,data):
+    def prepend(self, data):
         self.head = Node(data=data, next=self.head)
+
+    def add_to_end(self, data):
+        new_node = Node(data)
+        curr = self.head
+        while curr.next:
+            curr = curr.next
+        curr.next = new_node
+
+    def remove_from_head(self):
+        new_node = self.head
+        self.head = self.head.next
+        return new_node.data
+
+    def remove_from_end(self):
+        current = self.head
+        previous = self.head
+        while current.next:
+            previous = current
+            current = current.next
+        previous.next = None
+        return current.data
+
+    def clear(self):
+        self.head = None
+
+    def search(self,value):
+        curr = self.head
+        while curr.next:
+            if curr.data == value:
+                return True
+            curr = curr.next
+        if curr.data == value:
+                return True
+        else:
+            return False
+
+    def remove(self,value):
+        curr = self.head
+        prev = self.head
+        if self.head.data == value:
+            self.head = self.head.next
+            return curr.data
+        while curr.data == value or curr.next:
+            if curr.data == value:
+                prev.next = curr.next
+                return curr.data
+            prev = curr
+            curr = curr.next
